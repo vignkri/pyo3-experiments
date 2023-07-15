c813bb4fc796d44f96ab9d2c41a239acdbcc25cc
vignkri <vignesh@hybridgreentech.com>
2023-07-15 11:40:04 +0200

    Construct a specific object for handling python

---
+++ README.md
@@ -0,0 +1,8 @@
+# PyO3 Experiments
+
+Experiments in working with interoperability between Rust and Python using PyO3 and other internal libraries.
+
+## Notes
+
+- The byte array handling seems to work pretty seamlessly, in order to avoid data being copied over
+- pointers
--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,67 @@
 //! and passed from python using bytes that are parsed back.
 
 use pyo3::prelude::*;
+use std::{fs::File, io::Read, marker::PhantomData};
+
+struct PythonHandler<'a> {
+    entry_file: String,
+    phantom: PhantomData<&'a u8>,
+}
+
+impl<'a> PythonHandler<'a> {
+    fn new(path: &str) -> Self {
+        let mut locn = env!("CARGO_MANIFEST_DIR").to_owned();
+        locn.push_str(path);
+
+        // open file
+        let mut py_entrypoint = File::open(locn).expect("Unable to open file");
+        let mut file_contents = String::new();
+        py_entrypoint.read_to_string(&mut file_contents).unwrap();
+
+        Self {
+            entry_file: file_contents,
+            phantom: PhantomData,
+        }
+    }
+
+    fn run(&self) -> Result<Vec<u8>, PyErr> {
+        // generate a result from the python interface
+        let res = Python::with_gil(|python| -> Result<Vec<u8>, PyErr> {
+            // log the currently utilised version of python
+            let version_info = python.version_info();
+            println!("Python: {:?}", version_info);
+
+            // load the python versioning of the code
+            let app: &PyAny = PyModule::from_code(python, &self.entry_file, "", "")
+                .unwrap()
+                .getattr("handler")
+                .unwrap();
+
+            // run the code
+            let data = String::from("hello");
+            let argument_data = data.as_bytes();
+
+            let result: &PyAny = app.call((argument_data.into_py(python),), None)?;
+
+            // handle data conversions to the internal rust objects
+            let data: &[u8] = result.extract()?;
+            let byte_vec = data.iter().map(|v| *v).collect::<Vec<u8>>();
+
+            // return a valid byte vec
+            Ok(byte_vec)
+        });
+
+        res
+    }
+}
 
 fn main() {
-    // read the file structures and handle the application layer
-    let python_entry = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/cart/arrow.py"));
-
     // startup the gil for operations
     pyo3::prepare_freethreaded_python();
 
-    // generate a result from the python interface
-    let result_from_python = Python::with_gil(|python| -> Result<Vec<u8>, PyErr> {
-        // log the currently utilised version of python
-        let version_info = python.version_info();
-        println!("Python: {:?}", version_info);
-
-        // load the python versioning of the code
-        let app: &PyAny = PyModule::from_code(python, python_entry, "", "")
-            .unwrap()
-            .getattr("handler")
-            .unwrap();
-
-        // run the code
-        let data = String::from("hello");
-        let argument_data = data.as_bytes();
-
-        let result: &PyAny = app.call((argument_data.into_py(python),), None)?;
-
-        // handle data conversions to the internal rust objects
-        let data: &[u8] = result.extract()?;
-        let byte_vec = data.iter().map(|v| *v).collect::<Vec<u8>>();
-
-        // return a valid byte vec
-        Ok(byte_vec)
-    });
+    // read the file structures and handle the application layer
+    let snake = PythonHandler::new("/cart/entry.py");
+    let result_from_python = snake.run();
 
     // run from python
     match result_from_python {
